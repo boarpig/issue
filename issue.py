15d8848af71898f8ee71ec541a482a4e8144ecdb
Lauri Hakko <lauri.hakko@gmail.com>
2013-12-20 22:25:31 +0200

    added --tag

--- issue.py
+++ issue.py
@@ -17,13 +17,15 @@ def main():
 
     new_parser = subparsers.add_parser("new", help="Add new issue.")
     new_parser.add_argument("-m", "--message", 
-            help="Message for the issue. if omitted, the $EDITOR will be invoked.")
+            help="""Message for the issue. if omitted, the $EDITOR will be
+            invoked.""")
 
     list_parser = subparsers.add_parser("list", help="List issues")
-    list_parser.add_argument("-a", "--all", action="store_true", default=False,
-            help="List all issues instead of open ones.")
-    list_parser.add_argument("-c", "--closed", action="store_true", default=False,
-            help="List closed issues.")
+    list_parser.add_argument("-a", "--all", action="store_true", 
+            default=False, help="List all issues instead of open ones.")
+    list_parser.add_argument("-c", "--closed", action="store_true", 
+            default=False, help="List closed issues.")
+    list_parser.add_argument("-t", "--tag", help="List only specified tags")
 
     close_parser = subparsers.add_parser("close", help="Close an issue")
     close_parser.add_argument("number", type=int, help="Issue number to close")
