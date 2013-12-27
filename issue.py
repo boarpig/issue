c4614e94401eb151af43760e4fdebf2f6bd61a9f
Lauri Hakko <lauri.hakko@gmail.com>
2013-12-27 21:25:02 +0200

    moved issues loading to separate function

--- issue.py
+++ issue.py
@@ -297,10 +297,7 @@ def parse_arguments():
 
     return parser.parse_args()
     
-
-def main():
-    args = parse_arguments()
-
+def load_issues(args):
     global issues
     if exists("ISSUES"):
         try:
@@ -343,6 +340,10 @@ def main():
             print("You can create one with\n\n $ issue init\n")
             exit(1)
 
+
+def main():
+    args = parse_arguments()
+    load_issues(args)
     if args.subparser == "init":
         init(args.force, args.gzip)
     elif args.subparser == "add":
