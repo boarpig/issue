56bb85262bd798d5c91ac027d0108250589a5e47
Lauri Hakko <lauri.hakko@gmail.com>
2014-01-07 18:17:41 +0200

    added python 3.2 through 3.4

--- setup.py
+++ setup.py
@@ -23,6 +23,9 @@ setup(
             'Operating System :: POSIX :: Linux',
             'Programming Language :: Python',
             'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
             'Topic :: Software Development',
             'Topic :: Software Development :: Bug Tracking',
             'Topic :: Utilities'
